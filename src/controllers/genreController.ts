77ada383e5552781ba89d6dc13ebac96380fa743
matheus <wallysom2@gmail.com>
2023-04-09 05:25:16 -0300

    feat (genre): controllers

--- src/controllers/genreController.ts
+++ src/controllers/genreController.ts
@@ -3,16 +3,32 @@ import httpStatus from 'http-status';
 
 import genreService from '../services/genreService.js';
 
+export function getAllGenres (req: Request, res: Response) {
+    const genres = genreService.getAllGenres();
+    res.send (genres)
+}
+
 export function getGenreById (req: Request, res: Response) {
     const id = parseInt (req.params.id);
 
     try {
-        const genre = await genreService.getGenreById(id);
+        const genre =  genreService.getGenreById(id);
         res.send(genre);
         
     } catch (error) {
         console.log (error)
         res.sendStatus(httpStatus.NOT_FOUND);
+    }
+}
+
+export function createGenre (req: Request, res: Response) {
+    const genre = req.body;
+    try {
+        genreService.createGenre(genre);
+        res.sendStatus(httpStatus.CREATED);
+    } catch (error) {
+        console.log (error)
+        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
 }
 
